6c26207c875d4b4bf5a82c33be783ec50aeac3d4
Artem <nevadaltd88@gmail.com>
2021-12-22 17:44:33 +0200

    navbar
    creating structure navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar.js'
+import GlobalStyle from './globalStyle.js'
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Navbar />
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,88 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components/macro'
+import { menuData } from '../data/MenuData.js'
+import { Button } from './Button.js'
+import { FaBars } from 'react-icons/fa'
 
 const Nav = styled.nav`
  	height: 60px;
+ 	display: flex;
+ 	justify-content: space-between;
+ 	padding: 1rem 2rem;
+ 	z-index: 100;
+ 	position: fixed;
+ 	width: 100%;
  	background: teal;
- `
+ `;
+
+const NavLink = css`
+ 	color: white;
+ 	display: flex;
+ 	align-items: center;
+ 	padding: 0 1rem;
+ 	height: 100%;
+ 	cursor: pointer;
+ 	text-decoration: none;
+ `;
 
 const Logo = styled(Link)`
-	color: white;
- `
+	${NavLink}
+	font-style: italic;
+ `;
 
-const MenuBars = styled.i``;
+const MenuBars = styled(FaBars)`
+	display: none;
 
-const NavMenu = styled.div``;
+	@media screen and (max-width: 768px) {
+		display: block;
+		cursor: pointer;
+		top: 0;
+		right: 0;
+	}
+`;
+
+const NavBtn = styled.div`
+	display: flex;
+	align-items: center;
+	margin-right: 24px;
+
+	@media screen and (max-width: 768px) {
+		display: none;
+	}
+ `;
+
+const NavMenu = styled.div`
+	display: flex;
+	align-items: center;
+	margin-right: -48px;
+
+	@media screen and (max-width: 768px) {
+		display: none;
+	}
+ `;
+
+const NavMenuLinks = styled(Link)`
+ 	${NavLink}
+ `;
 
 const Navbar = () => {
 	return (
 		<Nav>
 			<Logo to='/'>There Logo</Logo>
 			<MenuBars />
-			<NavMenu />
-			<h1>Navbar</h1>
+			<NavMenu>
+				{menuData.map((item, index) => (
+					<NavMenuLinks to={item.link} key={index}>
+						{item.title}
+					</NavMenuLinks>
+				))}
+			</NavMenu>
+			<NavBtn>
+				<Button to='/contact' primary={true}>
+					Contact Us
+				</Button>
+			</NavBtn>
 		</Nav>
 	)
 }
