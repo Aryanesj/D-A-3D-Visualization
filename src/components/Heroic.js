5f164c84ab3d6a0ca788527fd7d3fa609b199707
Artem <nevadaltd88@gmail.com>
2021-12-23 18:49:54 +0200

    heroic
    more css/ + image

--- src/components/Heroic.js
+++ src/components/Heroic.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled, { сss } from 'styled-components/macro'
+import styled, {css} from 'styled-components/macro'
 import { Button } from './Button.js'
 import { IoMdArrowRoundForward } from 'react-icons/io'
 import { IoArrowForward, IoArrowBack } from 'react-icons/io5'
@@ -21,13 +21,71 @@ const HeroicWrapper = styled.div`
 	position: relative;
 `
 
-const HeroicSlide = styled.div``
+const HeroicSlide = styled.div`
+	z-index: 1;
+	width: 100%;
+	height: 100%;
+`
 
-const HeroicSlider = styled.div``
+const HeroicSlider = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 
-const HeroicImage = styled.img``
+	&::before {
+		content: '';
+		position: absolute; 
+		z-index: 2;
+		width: 100%;
+		height: 100vh;
+		bottom: 0vh;
+		left: 0;
+		overflow: hidden;
+		opacity: 0.4;
+		background: linear-gradient(0deg, rgba(0,0,0,0.2) 0%,
+													 rgba(0,0,0,0.2) 50%, 
+													 rgba(0,0,0,0.6) 100%)
 
-const HeroicContent = styled.div``
+	}
+`
+
+const HeroicImage = styled.img`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 100vh;
+	object-fit: cover;
+`
+
+const HeroicContent = styled.div`
+	position: relative;
+	z-index: 10;
+	display: flex;
+	flex-direction: column;
+	max-width: 1600px;
+	width: calc(100% - 100px);
+	color: white;
+
+	h1 {
+		font-size: clamp(1rem, 8vw, 2rem);
+		font-weight: 400;
+		text-transform: uppercase;
+		text-shadow: 0px 0px 20px rgba(0,0,0,0.4);
+		text-align: left;
+		margin-bottom: 0.8rem;
+	}
+
+	p {
+		margin-bottom: 1.2rem;
+		text-shadow: 0px 0px 20px rgba(0,0,0,0.4);
+	}
+`
 
 const Arrow = styled(IoMdArrowRoundForward)``
 
@@ -39,7 +97,7 @@ const SliderButtons = styled.div`
 	z-index: 10;
 `
 
-const arrowButtons = styled.css`
+const arrowButtons = css`
 	width: 50px;
 	height: 50px;
 	color: #fff;
@@ -74,7 +132,7 @@ const Heroic = ({ slides }) => {
 					return (
 						<HeroicSlide key={index}>
 							<HeroicSlider>
-								<HeroicImage />
+								<HeroicImage src={slide.image} alt={slide.alt}/>
 									<HeroicContent>
 										<h1>{slide.title}</h1>
 										<p>{slide.price}</p>
@@ -83,7 +141,7 @@ const Heroic = ({ slides }) => {
 												  style={{maxWidth: '160px'}}
 										>
 											{slide.label}
-										{<Arrow />}
+										<Arrow />
 										</Button>
 									</HeroicContent>
 							</HeroicSlider>
